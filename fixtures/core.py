590399a9dc3dadc217a7130a0622efa12d43f1e5
Maxim Podbolotov <podbolotov.maxim@gmail.com>
2025-02-06 16:08:07 +0500

    Создана фикстура, добавляющая информацию о системном окружении в отчёт;

--- fixtures/core.py
+++ fixtures/core.py
@@ -1,8 +1,27 @@
+from platform import python_version
+
 import allure
 import pytest
+import platform
 
 from database.db_baseclass import Database
 from helpers.varirable_manager import VariableManager
+from data.framework_variables import FrameworkVariables as FrVars
+
+
+@pytest.fixture(scope="session", autouse=True)
+@allure.title("Запись информации об окружении в отчёт")
+def report_environment_properties_generation():
+    f = open("allure-results/environment.properties", "a", encoding='utf-8')
+    current_platform = platform.system()
+
+    f.write(f"""
+    Platform={current_platform}
+    Python\\ version={python_version()}
+    Application\\ URL={FrVars.APP_HOST}
+    DBMS\\ URL={FrVars.DB_HOST}:{FrVars.DB_PORT}
+    """)
+    f.close()
 
 
 @pytest.fixture(scope="session", autouse=True)
